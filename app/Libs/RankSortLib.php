a177b8aa9fc69f93919b40d2988abfd930d69ff8
masaya <mizugasira@example.com>
2017-04-24 21:03:28 +0900

    mizugashira-mypage-ranking8

--- app/Libs/RankSortLib.php
+++ app/Libs/RankSortLib.php
@@ -6,22 +6,26 @@ class RankSortLib extends BaseGameLib
     public function ranksort( $inputrank, $userId )
     {
 	// $rankchange = filter_input(INPUT_COOKIE, 'rankchange');
-
+	var_dump($inputrank);
+	
 	// DB接続
 	$getrank = $this->Model->exec('Ranking', 'getByrank', $userId);
 	$sortrank = [];
 	
-	// 
 	$userrank = array_search($userId, array_column($getrank, 'userId'));
-
+	
 	// ランキング取得時、中間ではなく、上位十位以内だった場合
 	if($getrank[11]['userId'] != $userId && 10 > $userrank){
-	    echo 'no';
+	    
+
+	    foreach ($getrank as $key => $value) {
+		if($key < 10){
+		    $sortrank[$value['name']] = $value;
+		}
+    	    }
 	}
 
 	//arsort($sortrank, SORT_NUMERIC);
-	
-	//var_dump($sortrank);
 	
 	return $sortrank;
     }
--- resources/views/ranking.blade.php
+++ resources/views/ranking.blade.php
@@ -5,24 +5,18 @@
 <div>
     <form action="{{APP_URL}}ranking" method="get">
 	<?php foreach ($rank as $key => $value) : ?>
-	    <p> : {{$key}} : {{$value[0]}}pt</p>
-	    <br>
+	<p>{{$value['rank']}}位　：　{{$value['name']}}　：　{{$value['totalPoint']}}Pt :　{{$value['userId']}}</p>
+	
+	<?php if (10 < $rank[$key]['rank'] && $value == end($rank)) : ?>
+	    <button type='submit' name='fullback' value='0'> << </button>
+	    <button type='submit' name='back' value='{{$rank[$key]['rank']}}'>back</button>
+	<?php endif; ?>
 	    
-
-	    <?php if (10 < $rank[$key][1] && $value == end($rank)) : ?>
-		<button type='submit' name='fullback' value='0'> << </button>
-		<button type='submit' name='back' value='{{$rank[$key][1]}}'>back</button>
+	<?php if (count($rank) == 10 && $value == end($rank)) : ?>
+	    <?php if (filter_input(INPUT_COOKIE, "bottom") != $rank[$key]['totalPoint']) : ?>
+		<button type='submit' name='next' value='{{$rank[$key]['rank']}}'>next</button>
 	    <?php endif; ?>
-			    
-	    <?php if (count($rank) == 10 && $value == end($rank)) : ?>
-		<?php if (filter_input(INPUT_COOKIE, "bottom") != $rank[$key][0]) : ?>
-		    <button type='submit' name='next' value='{{$rank[$key][1]}}'>next</button>
-		<?php endif; ?>
-	    <?php endif;?>
-		    
-	    <?php if (end($rank) == $value && $rank[$key][0] != filter_input(INPUT_COOKIE, "bottom")) : ?>
-		<button type='submit' name='fullnext' value='{{filter_input(INPUT_COOKIE, "count")}}'> >> </button>
-	    <?php endif;?>
+	<?php endif;?>
 
 	<?php endforeach; ?>
 	    <br>
