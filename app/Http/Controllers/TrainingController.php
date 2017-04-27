6f24ec8169c1c78044fd3bc055183f51e35e8ac7
hidari <e.smile.sys.php@gmail.com>
2017-04-27 10:42:11 +0900

    pagecoading toppage newpage mypage trainingpage

---
+++ app/Http/Controllers/TrainingController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+//Model
+use App\Model\UserModel;
+//Lib
+use App\Libs\DevelopMemberLib;
+
+class TrainingController extends BaseGameController
+{
+    /**
+     * TOP画面表示
+     * 
+     */
+    public function index()
+    {
+        //configからとってくる
+        $membersConf          =   \Config::get('members.profile');
+
+        //Libraly
+        $this->viewData['memberList'] = DevelopMemberLib::sortMemberConf( $membersConf );
+
+        return viewWrap('training', $this->viewData);
+    }
+
+    /**
+     * ユーザーIDをチェックしてリダイレクト
+     *
+     * @param uid
+     * @return Redirect
+     */
+    public function login()
+    {
+	//DB更新
+	UserModel::createUser();
+
+	//リダイレクト
+	return $this->redirect('mypage', 'index');
+    }
+}
