782928780fccb9b5958c169ebaad8e928bfc6a63
utsumishoya <utushan.1203@gmail.com>
2017-05-10 18:29:22 +0900

    修正

--- resources/views/gachaselect.blade.php
+++ resources/views/gachaselect.blade.php
@@ -1,12 +1,23 @@
 <div>
+	<a href="{{APP_URL}}gacha/select">	通常　</a>	<a href="{{APP_URL}}gacha/eventsSelect">	イベント　</a>
+</div>
 	<form action="{{APP_URL}}gacha/viewDataSet" method="get">
 	<input type="image" src="{{IMG_URL_TEST}}gachabutton1.png" name = 'gachavalue' value = "1" width= 100% height= 100%>
 	<input type="image" src="{{IMG_URL_TEST}}gachabutton2.png" name = 'gachavalue' value = "2"width= 100% height= 100%>
 	<input type="image" src="{{IMG_URL_TEST}}gachabutton3.png" name = 'gachavalue' value = "3"width= 100% height= 100%>
-	</span>
-	<a href="{{APP_URL}}gacha/select">	通常　</a>	<a href="{{APP_URL}}gacha/eventsSelect">	イベント　</a>
-</div>
 </form>
+</div>
 
-
+{{-- popupボタン --}}
+<div class="modal_container">
+</div>
+{{-- popupウインドウ --}}
+<div class="modal {{'gacha'}}">
+	@include('popup/'.'gacha')
+		<div class="modal_frame">
+			<div class="close">
+		<span>close</span>
+	</div>
+</div>
+</div>
 
---
+++ resources/views/popup/gacha.blade.php
@@ -0,0 +1,9 @@
+スカウトしますか？
+
+
+<form name="gacha">
+<div>
+<input type="button" name="yes" value="はい"onClick="kakunin">
+<input type="button" name="no" value="いいえ"onClick="kakunin">
+</div>
+</form>
