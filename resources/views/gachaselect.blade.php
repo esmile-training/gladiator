bda0589c8221c233011b34c66a4a8e3d5491adf1
masaya <mizugasira@example.com>
2017-05-10 12:08:01 +0900

    ranking実装まで

--- resources/views/gachaselect.blade.php
+++ resources/views/gachaselect.blade.php
@@ -1,19 +1,9 @@
 <div>
-	<form action="{{APP_URL}}gacha/index" method="get">
-			<input type="image" src="{{IMG_URL_TEST}}gachabutton.png" name = 'gachavalue' value = "1" width= 100% height= 100%>
-</div>
-<div>
+	<form action="{{APP_URL}}gacha/viewDataSet" method="get">
+	<input type="image" src="{{IMG_URL_TEST}}gachabutton1.png" name = 'gachavalue' value = "1" width= 100% height= 100%>
 	<input type="image" src="{{IMG_URL_TEST}}gachabutton2.png" name = 'gachavalue' value = "2"width= 100% height= 100%>
-</div>
-<div>
-	<input type="image" src="{{IMG_URL_TEST}}gachabutton4.png" name = 'gachavalue' value = "7"width= 100% height= 100%>
-
-</div>
-<div>
-	<input type="image" src="{{IMG_URL_TEST}}gachabutton5.png" name = 'gachavalue' value = "6"width= 100% height= 100%>
-</div>
-<div>
-	<input type="image" src="{{IMG_URL_TEST}}gachabutton3.png"  name = 'gachavalue' value = "8"width= 100% height= 100%>
+	<input type="image" src="{{IMG_URL_TEST}}gachabutton3.png" name = 'gachavalue' value = "3"width= 100% height= 100%>
+	<a href="{{APP_URL}}gacha/select">	通常　</a>	<a href="{{APP_URL}}gacha/eventsSelect">	イベント　</a>
 </div>
 </form>
 
