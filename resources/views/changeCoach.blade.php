537012843b39c5c59680ba441ae4c7878415ffaa
naoto yoshida <yoshida-naoto@social-sharing.org>
2017-05-11 11:29:50 +0900

    画像倍率変更(yoshida)

--- resources/views/changeCoach.blade.php
+++ resources/views/changeCoach.blade.php
@@ -4,14 +4,14 @@
 <div>
 	<div>
 		<image src="{{CHAR_IMG_URL}}{{$viewData['coach']['imgId']}}.png" alt="コーチイメージ"<
-			name="imgId" value="{{$viewData['coach']['id']}}" width="100" height="100">{{$viewData['coach']['name']}}<br>
+			name="imgId" value="{{$viewData['coach']['id']}}" width="75" height="100">{{$viewData['coach']['name']}}<br>
 	</div>
 	<div>
 	↓
 	</div>
 	<div>
 		<image src="{{CHAR_IMG_URL}}{{$viewData['chara']['imgId']}}.png" alt="キャライメージ"<
-			name="imgId" value="{{$viewData['chara']['id']}}" width="100" height="100">{{$viewData['chara']['name']}}<br>
+			name="imgId" value="{{$viewData['chara']['id']}}" width="75" height="100">{{$viewData['chara']['name']}}<br>
 	</div>
 	<form action="changeCoach/changeCoach" method="get">
 		<input type="hidden" name="coachId" value="{{$viewData['coach']['id']}}">
