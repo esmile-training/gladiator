92ac9978602ca67eb7f4b5b822e22e3cda619b34
masaya <mizugasira@example.com>
2017-05-11 19:02:16 +0900

    codeの修正 bladeの書き換え

--- app/Libs/TicketLib.php
+++ app/Libs/TicketLib.php
@@ -11,9 +11,12 @@ class TicketLib extends BaseGameLib
 
 		$max = \Config::get('ticket.ticketMax');
 		
-		if($user['battleTicket'] == $max - 1){
+		if($user['battleTicket'] == $max - 1)
+		{
 			$this->Model->exec('user', 'firstLossTicket', array($user,$time));
-		}else{
+		}
+		else
+		{
 			$this->Model->exec('user', 'updateTicket', array($user));
 		}
 	}
@@ -23,17 +26,17 @@ class TicketLib extends BaseGameLib
      */
     public function confirmation($userId, $nowTime, $battleTicket, $ticketLossTime)
     {	
-	// チケットの上限値を取得
-	$maxticket = \Config::get('ticket','ticketMax');
-	
-	// 回復時間の30分を加算させる。
-	$recoveryTime = date("Y-m-d H:i:s",strtotime("$ticketLossTime +30 minute"));
-	
-	// チケット枚数が上限より小さい時と、現在の時間が回復時間を上回っているとき実行
-	if($battleTicket < $maxticket['ticketMax'] && $recoveryTime <= $nowTime)
-	{
-	    TicketLib::ticketRecovery($userId, $battleTicket, $recoveryTime);
-	}
+		// チケットの上限値を取得
+		$maxticket = \Config::get('ticket','ticketMax');
+
+		// 回復時間の30分を加算させる。
+		$recoveryTime = date("Y-m-d H:i:s",strtotime("$ticketLossTime +30 minute"));
+		
+		// チケット枚数が上限より小さい時と、現在の時間が回復時間を上回っているとき実行
+		if($battleTicket < $maxticket['ticketMax'] && $recoveryTime <= $nowTime)
+		{
+			TicketLib::ticketRecovery($userId, $battleTicket, $recoveryTime);
+		}
     }
     
     /*
@@ -41,9 +44,9 @@ class TicketLib extends BaseGameLib
      */
     public function ticketRecovery($userId, $battleTicket, $recovery)
     {
-	// チケットを1枚回復させる。
-	$ticket = $battleTicket + 1;
-	$updateData = [$userId, $ticket, $recovery];
-	$this->Model->exec('User', 'ticketRecovery', $updateData);
+		// チケットを1枚回復させる。
+		$ticket = $battleTicket + 1;
+		$updateData = [$userId, $ticket, $recovery];
+		$this->Model->exec('User', 'ticketRecovery', $updateData);
     }
 }
