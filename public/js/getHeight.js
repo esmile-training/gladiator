f4b461ba568d1b4835eb632820b782c9c1ffc1ad
utsumishoya <utushan.1203@gmail.com>
2017-05-18 12:11:52 +0900

    ガチャ修正

--- public/js/getHeight.js
+++ public/js/getHeight.js
@@ -3,7 +3,8 @@ $(function(){
     var footerHeight = $('.footerPosition > img').height();
     
     var main = document.getElementById( "main" );
-
+	headerHeight = headerHeight-(headerHeight/8);
+	 
     main.style.marginTop = headerHeight+'px';
     main.style.marginBottom = footerHeight + 'px';
 });
