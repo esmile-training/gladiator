cdcd23f2184079fb36cab8d676c6dab9ef932803
utsumishoya <utushan.1203@gmail.com>
2017-05-18 18:21:52 +0900

    確認

--- public/js/getHeight.js
+++ public/js/getHeight.js
@@ -1,10 +1,12 @@
 $(function(){
     var headerHeight = $('.headerPosition > img').height();
     var footerHeight = $('.footerPosition > img').height();
+	var mainHeight = $('#main').height();
     
     var main = document.getElementById( "main" );
 	headerHeight = headerHeight-(headerHeight/8);
 	 
     main.style.marginTop = headerHeight+'px';
     main.style.marginBottom = footerHeight + 'px';
+	main.style.height = mainHeight + 'px';
 });
