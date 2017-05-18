4048850a9d1ea6f00fef46acf20218f703e5fdc6
masaya <mizugasira@example.com>
2017-05-18 18:36:38 +0900

    確認

--- public/js/getHeight.js
+++ public/js/getHeight.js
@@ -1,9 +1,15 @@
 $(function(){
     var headerHeight = $('.headerPosition > img').height();
     var footerHeight = $('.footerPosition > img').height();
-    
+    console.log(headerHeight);
     var main = document.getElementById( "main" );
+    headerHeight = headerHeight-(headerHeight/8);
+    //footerHeight = footerHeight+(footerHeight/2);
 
-    main.style.marginTop = headerHeight+'px';
-    main.style.marginBottom = footerHeight + 'px';
+    main.style.marginTop = (headerHeight)+'px';
+    main.style.marginBottom = (footerHeight) + 'px';
+    
+    var mainHeight = $('.junban0').height();
+    console.log(mainHeight);
+    main.style.height = mainHeight+'px';
 });
