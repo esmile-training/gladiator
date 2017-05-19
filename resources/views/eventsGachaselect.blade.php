3931131345f446cdecbc1aff4df65bbf7480ed5d
utsumishoya <utushan.1203@gmail.com>
2017-05-19 10:45:07 +0900

    ガチャ選択画面修正

--- resources/views/eventsGachaselect.blade.php
+++ resources/views/eventsGachaselect.blade.php
@@ -1,32 +1,32 @@
 @include('common/css', ['file' => 'gachaselect'])
-<div class = "all">
+<div class = "gacha_all">
 	<div>
-		<img class="kanban" src="{{IMG_URL_GACHA}}kanban.png">
+		<img class="gacha_signboard" src="{{IMG_URL_GACHA}}kanban.png">
 	</div>
 	<div class = "junban0">
-		<img class="waku" src="{{IMG_URL_GACHA}}wakuevent.png">
+		<img class="gacha_frame" src="{{IMG_URL_GACHA}}wakuevent.png">
+		<a href="{{APP_URL}}gacha/select">
+			<input type="submit" class = "normalbutton">
+		</a>
 	</div>
 	{{-- popoupボタン --}}
 	<div class = "junban1">
 		<div class="modal_container">
 			<?php $w = date("w",strtotime($viewData['nowTime'])); (int)$w +=5; ?>
 			<?php if(date('Y-m-d',strtotime($viewData['createTime'])) < date('Y-m-d',strtotime($viewData['nowTime']))): ?>
-			<div class = "button1">
+			<div class = "gacha_button1">
 				<input type="image" class="modal_btn gacha4" src="{{IMG_URL_GACHA}}gachabutton4.png" name = 'gachavalue' value = "4" width= 100% height= 100%>
 			</div>
 			<?php endif; ?>
-			<div class = "button2">
+			<div class = "gacha_button2">
 				<input type="image" class="modal_btn gacha{{$w}}" src="{{IMG_URL_GACHA}}gachabutton{{$w}}.png" name = 'gachavalue' value = "{{$w}}"width= 100% height= 100%>
 			</div>
-			<div class = "button2">
+			<div class = "gacha_button3">
 				<input type="image" src="{{IMG_URL_GACHA}}gachabutton5.png" width= 100% height= 100%>
 			</div>
 		</div>
 	</div>
 </div>
-<a href="{{APP_URL}}gacha/select">
-	<input type="submit" class = "normalbutton">
-</a>
 {{-- popupウインドウ --}}
 <div class="modal gacha4">
 	@include('popup/'.'gacha', ['gachaId' => 4])
