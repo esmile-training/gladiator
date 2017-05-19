c4e5ff192e042e7d103e2ad066a6ee478e377eea
Gojo_Toshiya <gojo-toshiya@esmile-sys.com>
2017-05-19 12:27:02 +0900

    背景、ボタンの位置整理　中間

--- resources/views/battle.blade.php
+++ resources/views/battle.blade.php
@@ -2,27 +2,22 @@
 @include('common/battle')
 
 <div class="battle_background{{$viewData['EnemyData']['difficulty']}}">
-
-	<div class="difficulty">
-		<img src="{{IMG_URL_BATTLE}}difficulty{{$viewData['EnemyData']['difficulty']}}.png">
-	</div>
 	
-	{{--バトル終了のフラグが立っていなければ--}}
-	@if ($viewData['BattleData']['delFlag'] != 1)
-		<div class="surrender">
+	<div class="hedder">
+		<img src="{{IMG_URL_BATTLE}}difficulty{{$viewData['EnemyData']['difficulty']}}.png" class="difficulty_Name">
+
+		{{--バトル終了のフラグが立っていなければ--}}
+		@if ($viewData['BattleData']['delFlag'] != 1)
 			<form method="get" action="{{APP_URL}}battle/makeResultData" onsubmit="doSomething();return false;">
-				<input type="image" src="{{IMG_URL_BATTLE}}surrender.png" value="1" name="sub2">
+				<input type="image" src="{{IMG_URL_BATTLE}}surrender.png" value="1" name="sub2" class="surrender_Button">
 			</form>
-		</div>
-		<div class="help">
-			<img src="{{IMG_URL_BATTLE}}help.png">
+			<img src="{{IMG_URL_BATTLE}}help.png"  class="help_Button">
 			{{-- ヘルプのポップアップを表示するボタンにする --}}
 	<!--		<form method="get" action="{{APP_URL}}battle/makeResultData" onsubmit="doSomething();return false;">
 				<input type="image" src="{{IMG_URL_BATTLE}}help.png" value="1" name="sub2">
 			</form>-->
-		</div>
-
-	@endif
+		@endif
+	</div>
 
 	{{-- 敵HPの表示 --}}
 	<div class="enemy_Status">
@@ -83,12 +78,18 @@
 	{{-- バトル終了のフラグが立っていなければ次の攻撃の受け付け --}}
 	@if ($viewData['BattleData']['delFlag'] != 1)
 		{{-- それぞれのボタン表示 --}}
-		<img src="{{IMG_URL_BATTLE}}buttonBg.png">
-		<form method="get" action="{{APP_URL}}battle/updateBattleData" onsubmit="doSomething();return false;">
-			<input type="image" src="{{IMG_URL_BATTLE}}goo.png" value="1" name="sub1">　
-			<input type="image" src="{{IMG_URL_BATTLE}}cho.png" value= "2" name="sub1">　
-			<input type="image" src="{{IMG_URL_BATTLE}}paa.png" value= "3" name="sub1">　
-		</form>
+		<div class="button">
+			<img src="{{IMG_URL_BATTLE}}buttonBg.png" class="button_bg" >
+			<a href="{{APP_URL}}battle/updateBattleData?hand=1" class="button_goo_linkregion" >
+				<img src={{IMG_URL_BATTLE}}goo.png class="button_icon" >
+			</a>
+			<a href="{{APP_URL}}battle/updateBattleData?hand=2" class="button_cho_linkregion" >
+				<img src={{IMG_URL_BATTLE}}cho.png class="button_icon" >
+			</a>
+			<a href="{{APP_URL}}battle/updateBattleData?hand=3" class="button_paa_linkregion" >
+				<img src={{IMG_URL_BATTLE}}paa.png class="button_icon" >
+			</a>
+		</div>
 	@endif
 	{{-- 自キャラステータスの表示 --}}
 	<div class="player_Status">
@@ -102,3 +103,4 @@
 		<img src="{{IMG_URL_BATTLE}}charaBar.png">
 	</div>
 </div>
+</body>
