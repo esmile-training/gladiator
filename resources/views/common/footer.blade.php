c1a56cc57af60306c4eec56b759abe8e522253ba
masaya <mizugasira@example.com>
2017-05-19 16:38:27 +0900

    header,footerの完成

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -4,23 +4,17 @@
 
 					<img class="footerImg" src="{{FOOTER_IMG_URL}}footer.png" />
 					<ul class="iconPosition">
-						<li><a href="{{APP_URL}}mypage/index"><img class="" src="{{FOOTER_IMG_URL}}homeIcon.png" /></a></li>
-						<li><a href="{{APP_URL}}battle/selectBattleChara"><img class="" src="{{FOOTER_IMG_URL}}battleIcon.png" /></a></li>
-						<li><a href="{{APP_URL}}training/index"><img class="" src="{{FOOTER_IMG_URL}}traningIcon.png" /></a></li>
-						<li><a href="{{APP_URL}}gacha/select"><img class="" src="{{FOOTER_IMG_URL}}gachaIcon.png" /></a></li>
+						<li><a class="location" href="{{APP_URL}}mypage/index"><img class="image_change" src="{{FOOTER_IMG_URL}}homeIcon.png" /></a></li>		
+						<li><a class="location" href="{{APP_URL}}battle/selectBattleChara"><img class="image_change" src="{{FOOTER_IMG_URL}}battleIcon.png" /></a></li>
+						<li><a class="location" href="{{APP_URL}}training/index"><img class="image_change" src="{{FOOTER_IMG_URL}}traningIcon.png" /></a></li>
+						<li><a class="location" href="{{APP_URL}}gacha/select"><img class="image_change" src="{{FOOTER_IMG_URL}}gachaIcon.png" /></a></li>
 					</ul>
 				</div>
-<!--				
-				<ul>
-					<li><a href="{{APP_URL}}mypage/index">マイページ</a></li>
-					<li><a href="{{APP_URL}}battle/selectBattleChara">試合</a></li>
-					<li><a href="{{APP_URL}}training/index">訓練所</a></li>
-					<li><a href="{{APP_URL}}gacha/select">ガチャ</a></li>
-				</ul>-->
 			</footer>
         </div>
         <script type="text/javascript" src="{{APP_URL}}js/jquery-3.2.1.min.js"></script>
         <script type="text/javascript" src="{{APP_URL}}js/modal.js"></script>
 		<script type="text/javascript" src="{{APP_URL}}js/getHeight.js"></script>
+		<script type="text/javascript" src="{{APP_URL}}js/imgChange.js"></script>
     </body>
 </html>
