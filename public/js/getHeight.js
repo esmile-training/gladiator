f093c2901afcd89654fb1c9f132a29d4ec62ab32
masaya <mizugasira@example.com>
2017-05-19 18:38:19 +0900

    確認

--- public/js/getHeight.js
+++ public/js/getHeight.js
@@ -1,19 +1,16 @@
 $(function(){
-    // ヘッダーとフッターの高さの幅を取得
     var headerHeight = $('.headerPosition > img').height();
     var footerHeight = $('.footerPosition > img').height();
     
     var main = document.getElementById( "main" );
     
-    // ヘッダーの画像の影の部分を詰める
     headerHeight = headerHeight-(headerHeight/8);
     
-    // mainの上下の幅を空ける
-	main.style.marginBottom = footerHeight+'px';
+
     
-	var body =  $('body').height();
-	body = body - (headerHeight + footerHeight);
-	main.style.height = body+'px';
+    var body =  $('body').height();
+    body = body - (headerHeight + footerHeight);
+    main.style.height = body+'px';
 	
 
 });
