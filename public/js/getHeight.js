03e9b6684092f36bdf3a195d0fa55383eef01aa5
masaya <mizugasira@example.com>
2017-05-19 18:47:54 +0900

    getHeight.jsの変更

--- public/js/getHeight.js
+++ public/js/getHeight.js
@@ -3,10 +3,11 @@ $(function(){
     var footerHeight = $('.footerPosition > img').height();
     
     var main = document.getElementById( "main" );
-    
+
     headerHeight = headerHeight-(headerHeight/8);
     
-
+    main.style.marginTop = headerHeight+'px';
+    main.style.marginBottom = footerHeight+'px';
     
     var body =  $('body').height();
     body = body - (headerHeight + footerHeight);
