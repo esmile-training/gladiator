d516489457c14add3c2a7d68228b7f2587d01a1e
masaya <mizugasira@example.com>
2017-05-22 16:02:05 +0900

    img名変更

--- public/js/getHeight.js
+++ public/js/getHeight.js
@@ -1,6 +1,6 @@
 $(function(){    
-    var headerHeight = $('.headerPosition > img').height();
-    var footerHeight = $('.footerPosition > img').height();
+    var headerHeight = $('.headerPosition > .headerImg').height();
+    var footerHeight = $('.footerPosition > .footerImg').height();
     
     var main = document.getElementById( "main" );
 
