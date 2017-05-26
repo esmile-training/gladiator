711a6eea8a395143773e3a7f28e3d7c2bd3b47fa
Gojo_Toshiya <gojo-toshiya@esmile-sys.com>
2017-05-26 15:30:00 +0900

    hpバー追加

--- resources/views/popup/surrender.blade.php
+++ resources/views/popup/surrender.blade.php
@@ -1,27 +1,22 @@
 @include('common/css', ['file' => 'surrender'])
 
-<div>
-	降参しますか？
-</div>
-<div>
-	降参費用：{{$cost}}
-</div>
-<div>
-	<table border="0" class="surrender_button">
-		<tr>
-			<td width="25%"></td>
-			<td width="20%">
-				<a>
-					<img class="surrender_button_yes" src="{{IMG_URL}}popup/yes_Button.png">
-				</a>
-			</td>
-			<td width="10%"></td>
-			<td width="20%">
-				<a>
-					<img class="surrender_button_no" src="{{IMG_URL}}popup/no_Button.png">
-				</a>
-			</td>
-			<td width="25%"></td>
-		</tr>
-	</table>
-</div>
+降参しますか？
+降参費用：{{$cost}}
+
+<table border="0" class="surrender_button">
+	<tr>
+		<td width="25%"></td>
+		<td width="20%">
+			<a href="{{APP_URL}}battle/makeResultData">
+				<img class="surrender_button_yes" src="{{IMG_URL}}popup/yes_Button.png">
+			</a>
+		</td>
+		<td width="10%"></td>
+		<td width="20%">
+			<a>
+				<img class="surrender_button_no" src="{{IMG_URL}}popup/no_Button.png">
+			</a>
+		</td>
+		<td width="25%"></td>
+	</tr>
+</table>
