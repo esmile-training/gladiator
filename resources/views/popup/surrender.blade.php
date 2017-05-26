902625f85d2ee1d583549eae6937d75943efde44
Gojo_Toshiya <gojo-toshiya@esmile-sys.com>
2017-05-26 11:18:02 +0900

    降参、説明ボタンの押し込み、ポップアップ表示

--- resources/views/popup/surrender.blade.php
+++ resources/views/popup/surrender.blade.php
@@ -1,12 +1,7 @@
 降参しますか？
 
-@include('common/js', ['file' => 'gachapopup'])
 <div>
-	<a name="gacha">
-		<img class="gacha_popup" src = "http://esmile-sys.sakura.ne.jp/gladiator/img/popup/popupbutton.png" name="yes" value="はい" onclick="yesno(gachaId = {{$gachaId}})">
-		<p class = "gacha_popupfont">はい</p>
-	</a>
 	<a class="location modal_btn" href="{{APP_URL}}battle/makeResultData" >
-		<img class="image_change　modal_btn" src="{{IMG_URL}}battle/surrender.png">
+		<img src = "http://esmile-sys.sakura.ne.jp/gladiator/img/popup/popupbutton.png">
 	</a>
 </div>
