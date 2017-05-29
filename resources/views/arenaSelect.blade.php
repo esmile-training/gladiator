1cc381e62c5315d9a8434ffa3ad5bbb0133f3494
utsumishoya <utushan.1203@gmail.com>
2017-05-29 12:26:47 +0900

    arenaSelect.blade.php

--- resources/views/arenaSelect.blade.php
+++ resources/views/arenaSelect.blade.php
@@ -6,16 +6,18 @@
 
 {{-- css  --}}
 @include('common/css', ['file' => 'battleSelect'])
-
 {{--大会の一覧を表示する--}}
 <div class="battle_select">
 	<div class = "battle_selectfont">
 		闘技場を選択して下さい
 	</div>
 	@foreach($viewData['difficultyList'] as $arena)
-			<div class = "battle_select{{$arena['id']}}">
-				<img src="{{IMG_URL}}battle/difficulty{{$arena['id']}}.png">
-			</div>
+			<a href="{{APP_URL}}battle/preparationBattle?arenaDifficulty={{$arena['id']}}
+			&selectedCharaId={{$viewData['selectedCharaId']}}">
+				<div class = "battle_select{{$arena['id']}}">	
+					<img src="{{IMG_URL}}battle/difficulty{{$arena['id']}}.png">
+				</div>
+			</a>
 		<br>
 	@endforeach
 </div>
