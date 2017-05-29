42d4968774cecb20e6055247b4ade68d44e7454e
masaya <mizugasira@example.com>
2017-05-29 14:19:56 +0900

    menuの完成

--- resources/views/popup/menu.blade.php
+++ resources/views/popup/menu.blade.php
@@ -1,6 +1,4 @@
 <div>
-	<a class="location" href="{{APP_URL}}mypage/index"><img class="image_change menu_icon" src="{{FOOTER_IMG_URL}}homeIcon.png" /></a>
-	<a class="location" href="{{APP_URL}}mypage/index"><img class="image_change menu_icon" src="{{FOOTER_IMG_URL}}homeIcon.png" /></a>
-	<a class="location" href="{{APP_URL}}mypage/index"><img class="image_change menu_icon" src="{{FOOTER_IMG_URL}}homeIcon.png" /></a>
-	<a class="location" href="{{APP_URL}}mypage/index"><img class="image_change menu_icon" src="{{FOOTER_IMG_URL}}homeIcon.png" /></a>
+	<a class="location buttom_size" href="{{APP_URL}}mypage/index"><img class="image_change menu_icon" src="{{IMG_URL}}menu/status.png" /></a>
+	<a class="location buttom_size" href="{{APP_URL}}mypage/index"><img class="image_change menu_icon" src="{{IMG_URL}}menu/ranking.png" /></a>
 </div>
