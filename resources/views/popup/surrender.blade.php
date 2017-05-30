14327b3459cfccd60f1d8f708752e54f6a8f87e8
Gojo_Toshiya <gojo-toshiya@esmile-sys.com>
2017-05-30 17:05:08 +0900

    総合ランキングのバグ修正、ポップアップのいいえボタンの仕様追加

--- resources/views/popup/surrender.blade.php
+++ resources/views/popup/surrender.blade.php
@@ -1,27 +1,29 @@
 @include('common/css', ['file' => 'surrender'])
 
 <div>
-降参しますか？
-</div>
-<div>
-降参費用：{{$cost}}<br />
+	降参しますか？<br />
+	降参費用：{{$cost}}<br />
+	<div class="surrender_button">
+		<table border="0">
+			<tr>
+				<td width="25%"></td>
+				<td width="20%">
+					<a href="{{APP_URL}}battle/makeResultData">
+						<img class="surrender_button_yes image_change" src="{{IMG_URL}}popup/yes_Button.png">
+					</a>
+				</td>
+				<td width="10%"></td>
+				<td width="20%">
+					<div class="no">
+						<img class="image_change" src="{{IMG_URL}}popup/no_Button.png">
+						<span>no</span>
+					</div>
+				</td>
+				<td width="25%"></td>
+			</tr>
+		</table>
+	</div>
 </div>
 
-<table border="0" class="surrender_button">
-	<tr>
-		<td width="25%"></td>
-		<td width="20%">
-			<a href="{{APP_URL}}battle/makeResultData">
-				<img class="surrender_button_yes image_change" src="{{IMG_URL}}popup/yes_Button.png">
-			</a>
-		</td>
-		<td width="10%"></td>
-		<td width="20%">
-			<div class="no">
-				<img class="image_change" src="{{IMG_URL}}popup/no_Button.png">
-				<span>no</span>
-			</div>
-		</td>
-		<td width="25%"></td>
-	</tr>
-</table>
+<script type="text/javascript" src="{{APP_URL}}js/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="{{APP_URL}}js/no.js"></script>
