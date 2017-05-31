dd97005aae515e1aa2f5c066e9053e887d596316
masaya <mizugasira@example.com>
2017-05-31 17:04:18 +0900

    mypage修正

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -1,9 +1,8 @@
 {{-- css  --}}
 @include('common/css', ['file' => 'mypage'])
-
 <img class="mypage_borad" src="{{IMG_URL}}mypage/mypageboard.png" />
-@if(1 <= $viewData['delFlag']['count'])
+@if(!is_null($viewData['delFlag']['imgId']))
 <div class="user_chara">
-	<img src="{{IMG_URL_CHARA}}{{$viewData['user']['imgId']}}.png">
+	<img src="{{IMG_URL_CHARA}}{{$viewData['delFlag']['imgId']}}.png">
 </div>
 @endif
