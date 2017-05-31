502afbf5ecf330a06cca34f42cf22881546d2f90
masaya <mizugasira@example.com>
2017-05-31 13:02:23 +0900

    mypage修正・完成

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -4,7 +4,7 @@
 @if($viewData['user']['imgId'] == null)
 	
 @else
-<img class="mypage_borad" src="{{IMG_URL}}ranking/mypagegboard.png" />
+<img class="mypage_borad" src="{{IMG_URL}}mypage/mypageboard.png" />
 <div class="user_chara">
 	<img src="{{IMG_URL_CHARA}}{{$viewData['user']['imgId']}}.png">
 </div>
