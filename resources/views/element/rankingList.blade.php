b75a54b62eb0ea331a6f59b59ed9f30d64f973c7
masaya <mizugasira@example.com>
2017-06-06 17:16:12 +0900

    rankingのcss修正

--- resources/views/element/rankingList.blade.php
+++ resources/views/element/rankingList.blade.php
@@ -1,6 +1,6 @@
 <div><img class="ranking_back_image" src="{{IMG_URL}}ranking/rankingbackimage.jpg" /></div>
 
-	<img class="ranking_borad" src="{{IMG_URL}}ranking/rankingboard.png" />
+	<img class="ranking_board" src="{{IMG_URL}}ranking/rankingboard.png" />
 	{{-- 背景切り替え --}}
 	@if(isset($viewData['ranking'][0]['weeklyAward']))
 		<img class="ranking_panel" src="{{IMG_URL}}ranking/weekrankingwindow.png"/>
--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -1,6 +1,6 @@
 {{-- css  --}}
 @include('common/css', ['file' => 'mypage'])
-<img class="mypage_borad" src="{{IMG_URL}}mypage/mypageboard.png" />
+<img class="mypage_board" src="{{IMG_URL}}mypage/mypageboard.png" />
 @if(!is_null($viewData['delFlag']['imgId']))
 <div class="user_chara">
 	<img src="{{IMG_URL_CHARA}}{{$viewData['delFlag']['imgId']}}.png">
