4cf94f206c4a2e47708d538b6ee46b27ac02321f
kurino ryoma <kurino-ryoma@social-sharing.org>
2017-06-06 18:42:26 +0900

    不具合修正

--- resources/views/popup/coachSelect.blade.php
+++ resources/views/popup/coachSelect.blade.php
@@ -1,19 +1,33 @@
 <div class="training_text">
 	{{'訓練時間を選択してください'}}
 </div>
-
 <div>
-	<script type="text/javascript">	
+	<script type="text/javascript">
+		
 		var uCoachHp		= 0;
 		var trainingFee		= 0;
+		var userMoney		= 0;
 
 		$(function()
 		{
 			uCoachHp	= <?php echo $viewData['coachList'][$uCoach]['hp']; ?>;
 			trainingFee = uCoachHp * 10;
 			
-			trainingFee{{$cnt}}				= document.getElementById("trainingFee{{$cnt}}");
-			trainingFee{{$cnt}}.innerHTML	= trainingFee;
+			// 所持金
+			userMoney = <?php echo $viewData['user']['money']; ?>;
+			
+			trainingFee{{$cnt}}	= document.getElementById("trainingFee{{$cnt}}");
+			trainingFee{{$cnt}}.innerHTML = Math.floor(trainingFee);
+			
+			// input class 取得
+			var inputClass= $('.training_submit');
+			
+			// 
+			if(userMoney < trainingFee{{$cnt}})
+			{
+				inputClass.prop('disabled', true);
+			}
+			
 		});
 
 		function feeCalcuration{{$cnt}}(select)
@@ -22,8 +36,11 @@
 			uCoachHp	= <?php echo $viewData['coachList'][$uCoach]['hp']; ?>;
 			trainingFee = uCoachHp * 10 * time * (100 - (time - 1) * 3) / 100;
 			
-			trainingFee{{$cnt}}				= document.getElementById("trainingFee{{$cnt}}");
-			trainingFee{{$cnt}}.innerHTML	= Math.floor(trainingFee);
+			userMoney = <?php echo $viewData['user']['money']; ?>;
+			
+			trainingFee{{$cnt}}	= document.getElementById("trainingFee{{$cnt}}");
+			trainingFee{{$cnt}}.innerHTML = Math.floor(trainingFee);
+			
 		}
 	</script>
 </div>
@@ -33,27 +50,27 @@
 		<img src="{{IMG_URL}}/user/gold.png">
 	</div>
 	<div class="coachSelect_text training_fee_text">
-		<p id="trainingFee{{$cnt}}"><p>
+		<p id="trainingFee{{$cnt}}"></p>
 	</div>
-</body>
+	
+	<form name='trainingInfo' action='{{APP_URL}}training/infoSet' method='get'>
+		<select name="trainingTime" onchange="feeCalcuration{{$cnt}}(this)">
+			<option>1</option>
+			<option>2</option>
+			<option>3</option>
+			<option>4</option>
+			<option>5</option>
+			<option>6</option>
+			<option>7</option>
+			<option>8</option>
+			<option>9</option>
+			<option>10</option>
+		</select>
 
-<form name="trainingInfo" action="{{APP_URL}}training/infoSet" method="get">
-	<select name="trainingTime" onchange="feeCalcuration{{$cnt}}(this)">
-		<option>1</option>
-		<option>2</option>
-		<option>3</option>
-		<option>4</option>
-		<option>5</option>
-		<option>6</option>
-		<option>7</option>
-		<option>8</option>
-		<option>9</option>
-		<option>10</option>
-	</select>
-	
-	<input class="training_submit" type="image" src="{{IMG_URL}}/popup/ok_Button.png">
-	
-	<input type = "hidden" name = "uCoachId" value = "{{$viewData['coachList'][$uCoach]['id']}}">
-	<input type = "hidden" name = "uCharaId" value = "{{$viewData['uCharaId']}}">
-	<input type = "hidden" name = "uCoachHp" value = "{{$viewData['coachList'][$uCoach]['hp']}}">
-</form>
+		<input id="hoge{{$cnt}}" class="training_submit" type="image" src="{{IMG_URL}}/popup/ok_Button.png" disabled="false">
+			
+		<input type = "hidden" name = "uCoachId" value = "{{$viewData['coachList'][$uCoach]['id']}}">
+		<input type = "hidden" name = "uCharaId" value = "{{$viewData['uCharaId']}}">
+		<input type = "hidden" name = "uCoachHp" value = "{{$viewData['coachList'][$uCoach]['hp']}}">
+	</form>
+</body>
