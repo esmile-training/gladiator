8b8491cef277075669bbe78f1493673ff20ca7bd
kurino ryoma <kurino-ryoma@social-sharing.org>
2017-06-06 11:35:13 +0900

    画像の追加、一部フロント部分の修正

--- resources/views/coachSelect.blade.php
+++ resources/views/coachSelect.blade.php
@@ -10,32 +10,23 @@
 	@foreach($viewData['coachList'] as $val)
 		<div class="training_coach_window">
 			@if($val['state'] == 1)
-				<div class ="modal_btn trainingPopup{{$cnt}} training_a_none">
-					<div class="scale_img"><img src="{{IMG_URL}}/training/coachButton.png"></div>
+			<div class ="modal_btn trainingPopup{{$cnt}} training_a_none">
+				<div class="scale_img"><img src="{{IMG_URL}}/training/coachButton.png"></div>
 			@else
-				<div class ="modal_btn trainingPopup{{$cnt}}">
-					<img src="{{IMG_URL}}/training/coachButton.png">
-			@endif	
+			<div class ="modal_btn trainingPopup{{$cnt}}">
+			@endif
+				<img src="{{IMG_URL}}/training/coachButton{{$val['rare']}}.png">
 				<div class="coachSelect_text training_coach_name">
 					<font>{{$val['name']}}</font>
 				</div>
 				<div class="training_coach_icon">
 					<img src="{{IMG_URL}}/chara/icon/icon_{{$val['imgId']}}.png">
 				</div>
-				<div class="training_goo_icon">
-					<img src="{{IMG_URL}}/chara/status/hand1.png">
-				</div>
 				<div class="coachSelect_text training_coach_goo_atk">
 					{{$val['gooAtk']}}
 				</div>
-				<div class="training_cho_icon">
-					<img src="{{IMG_URL}}/chara/status/hand2.png">
-				</div>
 				<div class="coachSelect_text training_coach_cho_atk">
 					{{$val['choAtk']}}
-				</div>
-				<div class="training_paa_icon">
-					<img src="{{IMG_URL}}/chara/status/hand3.png">
 				</div>
 				<div class="coachSelect_text training_coach_paa_atk">
 					{{$val['paaAtk']}}
