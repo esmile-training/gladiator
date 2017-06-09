750718d84a0f7bc372aa04d0df07cd9d743d8eec
matsui yuki <matsui-yuki@social-sharing.org>
2017-06-09 17:52:28 +0900

    キャラの一覧表示を行うCSSを作成

--- resources/views/charaSelect.blade.php
+++ resources/views/charaSelect.blade.php
@@ -1,67 +1,41 @@
 {{-- css  --}}
 @include('common/css', ['file' => 'coachSelect'])
 @include('common/css', ['file' => 'training'])
-@include('common/css', ['file' => 'charaSelect'])
-@include('common/css', ['file' => 'battleCharaSelect'])
+@include('common/css', ['file' => 'charaList'])
 @include('common/js',['file' => 'sort'])
-<div><img class="charaselect_back" src="{{IMG_URL}}battle/chara_select_bg.jpg" /></div>
+<div><img class="chara_list_background" src="{{IMG_URL}}battle/chara_select_bg.jpg" /></div>
 <?php
 	$type = isset($_GET['type']) ? $_GET['type'] : '';
 	$order = isset($_GET['order']) ? $_GET['order'] : '';
 ?>
 
-<div id="roadCover" class="offCover">
+<div id="loadCover" class="offCover">
 	<div>
 		<p>「<?php $order == 'DESC' ? print '降順' : print '昇順'; ?>」<br>「<?php echo 'ソート条件' ?>」<br>の条件で並べかえています。</p>
 	</div>
 </div>
-	<div class="sort_Box">
-	<form action="{{APP_URL}}charaSelect/index" method="get">
-		<ul>
-			<li>
-				<input type="radio" name="order" value="DESC" <?php $order == 'DESC' ? print 'class="act" checked' : ''; ?> onchange="submit(this.form),onSortChange(),getType()">
-				<label>昇順</label>
-			</li>
-			<li>
-				<input type="radio" name="order" value="ASC" <?php $order == 'ASC' ? print 'class="act" checked' : ''; ?> onchange="submit(this.form),onSortChange(),getType()">
-				<label>降順</label>
-			</li>
-		</ul>
-		<select name="type" onchange="submit(this.form),onSortChange(),getType()">
-			<option value="id"<?php $type == 'id' ? print 'selected' : ''; ?>>入手</option>
-			<option value="hp"<?php $type == 'hp' ? print 'selected' : ''; ?>>体力</option>
-			<option value="name"<?php $type == 'name' ? print 'selected' : ''; ?>>名前</option>
-			<option value="rare"<?php $type == 'rare' ? print 'selected' : ''; ?>>レア度</option>
-			<option value="gooAtk"<?php $type == 'gooAtk' ? print 'selected' : ''; ?>>グー 攻撃力</option>
-			<option value="choAtk"<?php $type == 'choAtk' ? print 'selected' : ''; ?>>チョキ 攻撃力</option>
-			<option value="paaAtk"<?php $type == 'paaAtk' ? print 'selected' : ''; ?>>パー 攻撃力</option>
-		</select>
-	</form>
-	</div>
-<div class="chara_list">
-	<div class="training_signboard_info">
-		<img src="{{IMG_URL}}/training/signboard_info.png">
-		<div class ="training_signboard_text">
-			<font  class="training_text">{{'剣闘士の詳細が見れます。'}}</font>
+
+	<div class="chara_list">
+		<div class="signboard_info">
+			<img src="{{IMG_URL}}/training/signboard_info.png">
+			<font  class="signboard_text font_serif font_color">{{'剣闘士の詳細が見れます'}}</font>
 		</div>
-	</div>
-	<div class="coachSelect_signboard">
-		 <img src="{{IMG_URL}}/status/statusSign.png">
-	</div>
-	
+		<div class="signboard">
+			 <img src="{{IMG_URL}}/status/statusSign.png">
+		</div>
+
 	{{--所持キャラクターをすべて表示する--}}
 @if(is_null($viewData['charaList']))
 	<div><p style="color: white">キャラクターがいません。</p></div>
 @else
-
 	<?php $count = 0; ?>
 	@foreach($viewData['charaList'] as $chara)
 		{{-- popupボタン --}}
 		{{--ボタンの表示間隔--}}
-		<div class="button_margin">
+		<div class="chara_button_margin">
 		{{--キャラボタン--}}
-		<div class="modal_btn charaStatus{{ $count }} chara_button" style='left: 2%'>
-			<input type='image' class="chara_button_frame_img" src='{{IMG_URL}}battle/chara_button_frame{{$chara['rare']}}.png'>
+		<div class="modal_btn charaStatus{{ $count }} chara_button">
+			<input type='image' class="chara_button_frame" src='{{IMG_URL}}battle/chara_button_frame{{$chara['rare']}}.png'>
 			{{--キャラアイコン--}}
 			<div class="chara_icon">
 				{{--キャラアイコン--}}
@@ -73,13 +47,13 @@
 			{{--ステータスの表示--}}
 			<div class="chara_status">
 				{{--名前の表示--}}
-				<font class="chara_name font_serif white">{{$chara['name']}}</font>
+				<font class="chara_name font_serif font_color">{{$chara['name']}}</font>
 				{{--hpの表示--}}
-				<font class="hp_value font_color_green font_sentury white">{{$chara['hp']}}</font>
+				<font class="hp_value font_sentury font_color">{{$chara['hp']}}</font>
 				{{--手の表示--}}
-				<font class="goo_value font_sentury white">{{$chara['gooAtk']}}</font>
-				<font class="cho_value font_sentury white">{{$chara['choAtk']}}</font>
-				<font class="paa_value font_sentury white">{{$chara['paaAtk']}}</font>
+				<font class="goo_value font_sentury font_color">{{$chara['gooAtk']}}</font>
+				<font class="cho_value font_sentury font_color">{{$chara['choAtk']}}</font>
+				<font class="paa_value font_sentury font_color">{{$chara['paaAtk']}}</font>
 			</div>
 		</div>
 	</div>
@@ -105,6 +79,29 @@
 		</div>
 	</div>
 	<?php $count++ ?>
+	<div class="sort_Box">
+		<form action="{{APP_URL}}charaSelect/index" method="get">
+			<ul>
+				<li>
+					<input type="radio" name="order" value="DESC" <?php $order == 'DESC' ? print 'class="act" checked' : ''; ?> onchange="submit(this.form),onSortChange(),getType()">
+					<label>昇順</label>
+				</li>
+				<li>
+					<input type="radio" name="order" value="ASC" <?php $order == 'ASC' ? print 'class="act" checked' : ''; ?> onchange="submit(this.form),onSortChange(),getType()">
+					<label>降順</label>
+				</li>
+			</ul>
+			<select name="type" onchange="submit(this.form),onSortChange(),getType()">
+				<option value="id"<?php $type == 'id' ? print 'selected' : ''; ?>>入手</option>
+				<option value="hp"<?php $type == 'hp' ? print 'selected' : ''; ?>>体力</option>
+				<option value="name"<?php $type == 'name' ? print 'selected' : ''; ?>>名前</option>
+				<option value="rare"<?php $type == 'rare' ? print 'selected' : ''; ?>>レア度</option>
+				<option value="gooAtk"<?php $type == 'gooAtk' ? print 'selected' : ''; ?>>グー 攻撃力</option>
+				<option value="choAtk"<?php $type == 'choAtk' ? print 'selected' : ''; ?>>チョキ 攻撃力</option>
+				<option value="paaAtk"<?php $type == 'paaAtk' ? print 'selected' : ''; ?>>パー 攻撃力</option>
+			</select>
+		</form>
+	</div>
 	@endforeach
 @endif
 </div>
