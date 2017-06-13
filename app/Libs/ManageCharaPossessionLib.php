6285bbeb4b2b59024cb676945c3eaa6cb00e43b8
matsui yuki <matsui-yuki@social-sharing.org>
2017-06-13 11:18:28 +0900

    所持キャラ数の制限用のライブラリを追加

---
+++ app/Libs/ManageCharaPossessionLib.php
@@ -0,0 +1,88 @@
+<?php
+/*
+キャラクターの所持制限処理のライブラリ
+作成者：松井勇樹
+最終更新日：2017/06/13
+*/
+// 名前空間
+namespace App\Libs;
+
+// クラスの定義
+class ManagePossessionLib extends BaseGameLib
+{
+	/*
+		ユーザーの所持キャラの上限数を取得する
+	*/
+	public function getUpperLimitChara($argUserId)
+	{
+		// DBからキャラ所持数の上限を取得する
+		$upperLimit = $this->Model->exec('user', 'getupperLimitChara', $argUserId);
+		return $upperLimit;
+	}
+
+	/*
+		所持キャラ数を取得する
+	*/
+	public function getPossessionChara($argUserId)
+	{
+		// DBからキャラ所持数を取得する
+		$PossessionChara = $this->Model->exec('user', 'getPossessionChara', $argUserId);
+		return $PossessionChara;
+	}
+
+	/*
+		キャラの所持数を加算する
+	*/
+	public function addPossessionChara($argUser)
+	{
+		// 所持キャラ数に1を加算する
+		$argUser['possessionChara'] += 1;
+		// データベースの更新をする
+		$this->Model->exec('user','UpdatePossessionChara',array($argUser));
+	}
+
+	/*
+		キャラの所持数を減算する
+	*/
+	public function subPossession($argUser)
+	{
+		// 所持キャラ数に1を加算する
+		$argUser['possessionChara'] -= 1;
+		// データベースの更新をする
+		$this->Model->exec('user','UpdatePossessionChara',array($argUser));
+	}
+
+	/*
+		キャラクターの所持枠を追加する
+	*/
+	public function addUpperLimit($argUser)
+	{
+		// 所持キャラ数の上限に10(仮)を加算する
+		$argUser['upperLimitChara'] += 10;
+		// データベースを更新する
+		$this->Model->exec('user','UpdateUpperLimitChara',array($argUser));
+	}
+
+	/*
+		キャラの所持数が上限を超えているかを確認する
+		超えていればtrue,超えていなければfalseを返す
+	*/
+	public function checkUpperLimit($argUser)
+	{
+		// キャラの所持数を取得する
+		$possession = getPossessionChara($argUserId);
+		// キャラ所持数の上限を取得する
+		$upperLimit = getUpperLimitChara($argUserId);
+
+		// 現在の所持数+1が、所持上限を超えていないか
+		if($possession + 1 >= $upperLimit)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+
+	}
+}
