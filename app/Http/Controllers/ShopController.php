a23e2a074807b46108a3702039e7b930e8c74fba
matsui yuki <matsui-yuki@social-sharing.org>
2017-06-19 12:49:06 +0900

    config/wordCheckList.phpにNGワードを追加

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -20,7 +20,7 @@ class ShopController extends BaseGameController
 
 		return viewWrap('shop', $this->viewData);
 	}
-	
+
 	/*
 	 * 所持アイテムや商品データを読み込むファンクション
 	 */
@@ -28,10 +28,10 @@ class ShopController extends BaseGameController
 	{
 		// 商品データ取得
 		$this->productData		= \Config::get('shop.productStr');
-		
+
 		// アイテムデータ取得
 		$this->itemData			= \Config::get('item.itemStr');
-		
+
 		// 所持アイテムデータ取得
 		$this->belongingsData	= $this->Model->exec('Item', 'getItemData', $this->user['id']);
 		// 所持アイテムデータがなければ作成
@@ -39,7 +39,7 @@ class ShopController extends BaseGameController
 		{
 			// uItemにデータを追加
 			$this->Model->exec('Item','insertItemData',$this->user['id']);
-			
+
 			// 所持アイテムデータ取得
 			$this->belongingsData	= $this->Model->exec('Item', 'getItemData', $this->user['id']);
 		}
@@ -55,25 +55,19 @@ class ShopController extends BaseGameController
 
 		// bladeから購入したアイテムのidを読み込み
 		$purchaseItemId = $_GET["purchaseItemId"];
-		
+
 		// bladeから購入したアイテムの個数を読み込み
 		$number			= $_GET["number"];
-		
+
 		// bladeから購入したアイテムの合計金額を読み込み
 		$totalPrice		= $_GET["totalPrice"];
-		
-		var_dump($purchaseItemId);
-		var_dump($number);
-		var_dump($this->itemData);
-		var_dump($this->belongingsData);
-		var_dump($this->user);exit;
 
 		// purchaseItemID = 1(チケット)ではない場合
 		if($purchaseItemId != 1)
-		{	
+		{
 			// DB更新用のデータ生成
 			$updateItemData = $this->Lib->exec('Belongings', 'updateItem', array($purchaseItemId, $number, $this->itemData, $this->belongingsData, $this->user));
-			
+
 			// DBの更新
 			$this->Model->exec('Item', 'updateItemData', array($updateItemData));
 		}
@@ -82,10 +76,10 @@ class ShopController extends BaseGameController
 			// チケットの回復
 			$this->Lib->exec('Ticket', 'recoveryTicket', array($this->user, $number));
 		}
-		
+
 		// ユーザーの所持金 'money' から降参費用を減算しデータベースに格納
 		$this->Lib->exec('Money', 'Subtraction', array($this->user,	$totalPrice));
-		
+
 		return $this->Lib->redirect('shop', 'index');
 	}
 }
