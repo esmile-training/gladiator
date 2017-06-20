dd10d3e6862cc6c3ed1534a8dcd87a5581d84092
utsumishoya <utushan.1203@gmail.com>
2017-06-20 12:40:02 +0900

    画像追加

--- resources/views/battleResult.blade.php
+++ resources/views/battleResult.blade.php
@@ -5,9 +5,11 @@
 
 		{{-- バトルの勝敗によって背景画像変更 --}}
 		@if ($viewData['prize'] > 0)
-			<div><img src="{{IMG_URL}}battleResult/battleResult_Bg_Win.jpg" class="battleresult_bg_win"></div>
+			<div><img src="{{IMG_URL}}battleResult/battleBack_Win.jpg" class="battleresult_bg_win"></div>
+			<div><img src="{{IMG_URL}}battleResult/battleLogo_Win.png" class="battleresult_logo_win"></div>
 		@else
-			<div><img src="{{IMG_URL}}battleResult/battleResult_Bg_Lose.jpg" class="battleresult_bg_lose"></div>
+			<div><img src="{{IMG_URL}}battleResult/battleBack_Lose.jpg" class="battleresult_bg_lose"></div>
+			<div><img src="{{IMG_URL}}battleResult/battleLogo_Lose.png" class=""></div>
 		@endif
 
 		{{-- リザルトログ表示領域 --}}
